853c2edbd0ff37d283d5afc1610f0cd1b81d9f35
Frixionburne <frixionburne@gmail.com>
2021-02-27 14:58:59 -0500

    updates

--- CharacterConfigs/Sorceress.rusher.js
+++ CharacterConfigs/Sorceress.rusher.js
@@ -151,7 +151,7 @@ function LoadConfig() {
 	*/
 
 	Config.Leader = ""; // Leader's ingame character name. Leave blank to try auto-detection (works in AutoBaal, Wakka, MFHelper)
-	Config.QuitList = [""]; // List of character names to quit with. Example: Config.QuitList = ["MySorc", "MyDin"];
+	Config.QuitList = ["frixlsI", "frixlsII", "frixlsIII", "frixlsIV"]; // List of character names to quit with. Example: Config.QuitList = ["MySorc", "MyDin"];
 	Config.QuitListMode = 0; // 0 = use character names; 1 = use profile names (all profiles must run on the same computer).
 	Config.QuitListDelay = []; // Quit the game with random delay in case of using Config.QuitList. Example: Config.QuitListDelay = [1, 10]; will exit with random delay between 1 and 10 seconds.
 
